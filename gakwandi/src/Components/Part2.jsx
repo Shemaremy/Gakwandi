0bc5853718315ef0de89925d9aadb388e0d16170
Shemaremy <remyshema20@gmail.com>
2024-10-11 15:08:48 +0200

    Removed comments

--- gakwandi/src/Components/Part2.jsx
+++ gakwandi/src/Components/Part2.jsx
@@ -4,12 +4,8 @@ import './Mods.css';
 
 function Part2() {
   return (
-    // Hero Section
     <section id="hero" className="hero section dark-background">
-      {/* <img src="assets/img/hero-bg.jpg" alt="" data-aos="fade-in" /> */}
-      {/* <img className="landing-img" src="assets/img/Landing/Furniture-landing.jpg" alt="" data-aos="fade-in" /> */}
       <img className="landing-img" src="assets/img/Landing/Furniture-landing2.jpg" alt="" data-aos="fade-in" />
-      {/* <img className="landing-img" src="assets/img/Landing/Mountains.jpg" alt="" data-aos="fade-in" /> */}
 
       <div className="container">
         <div className="row justify-content-center text-center" data-aos="fade-up" data-aos-delay="100">
@@ -22,14 +18,12 @@ function Part2() {
         <div className="row gy-4 mt-5 justify-content-center" data-aos="fade-up" data-aos-delay="200">
           <div className="col-xl-2 col-md-4" data-aos="fade-up" data-aos-delay="300">
             <div className="icon-box">
-              {/* <i className="bi bi-binoculars"></i> */}
               <i className="fa-solid fa-check"></i>
               <h3><a href="">High Quality</a></h3>
             </div>
           </div>
           <div className="col-xl-2 col-md-4" data-aos="fade-up" data-aos-delay="400">
             <div className="icon-box">
-              {/* <i className="bi bi-bullseye"></i> */}
               <i className="fa-solid fa-sack-dollar"></i>
               <h3><a href="">Affordability</a></h3>
             </div>
@@ -42,14 +36,12 @@ function Part2() {
           </div>
           <div className="col-xl-2 col-md-4" data-aos="fade-up" data-aos-delay="600">
             <div className="icon-box">
-              {/* <i className="bi bi-card-list"></i> */}
               <i className="fa-regular fa-face-smile"></i>
               <h3><a href="">Customer Satisfaction</a></h3>
             </div>
           </div>
           <div className="col-xl-2 col-md-4" data-aos="fade-up" data-aos-delay="700">
             <div className="icon-box">
-              {/* <i className="bi bi-gem"></i> */}
               <i className="fa-solid fa-truck"></i>
               <h3><a href="">Reliable Delivery</a></h3>
             </div>
